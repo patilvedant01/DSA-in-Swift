8a03f5524818c6283c656c378c51db4cd09284e6
patilvedant01 <vedantpatil324@gmail.com>
2024-04-20 14:46:12 +0530

    Initial Commit

---
+++ DSA in Swift/DSA_in_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSA_in_SwiftApp.swift
+//  DSA in Swift
+//
+//  Created by Vedant Patil on 20/04/24.
+//
+
+import SwiftUI
+
+@main
+struct DSA_in_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
